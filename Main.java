a1bca6a2c96e7287d99ec13973d23e4794788ba6
alilinykh <aililnykh@dtcc.edu>
2019-05-21 18:22:06 -0400

    lab Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please, enter your name");
+        String userName = userInput.nextLine();
+        if(userName.equalsIgnoreCase("alice") ||userName.equalsIgnoreCase("bob")){
+            System.out.printf("Hello, %s! Welcome!",userName);
+        }
+        else {
+            System.out.println("Hello, stranger!");
+        }
     }
+            
+   
 }
